8b32b77d0efcc1bb7ca440bb39e05cb65f378a64
Megha-Praveen <110711257+Megha-Praveen@users.noreply.github.com>
2025-08-11 20:15:40 +0530

    Version 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-primary shadow-sm py-2 sticky-top border-bottom border-primary">
       <Container fluid>
         <Navbar.Brand href="#home" className="d-flex align-items-center text-white fw-bold fs-4">
-          <img src="/src/assets/logo.png"  alt="Logo"  width="45"  height="45"  className="me-2 rounded-circle border-2 border-primary"/>
+          <img src={logo}  alt="Logo"  width="45"  height="45"  className="me-2 rounded-circle border-2 border-primary"/>
           Track Moving Spares
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" className="bg-primary" />
